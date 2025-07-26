6096a8ead45483edb84d9c03d1488cb2726c2ce3
Pro696969 <krishnakumaarmc@gmail.com>
2025-07-27 01:15:37 +0530

    modified buildenv, added cicd files
    Fixed hardcoded file paths in buildenv. Added .github/ cicd-dev.groovy cicd-stable.groovy

---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/RPMport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/RPMport.git'), string(name: 'PORT_DESCRIPTION', value: 'The RPM package manager' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
